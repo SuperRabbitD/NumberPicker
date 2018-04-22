f9598df06bbea991cccdb21f384932dd739cd1af
WangLu <lw0203jojo@gmail.com>
2018-04-22 14:38:41 -0500

    remove test code

--- app/src/main/java/com/wang/demo/MainActivity.java
+++ app/src/main/java/com/wang/demo/MainActivity.java
@@ -18,40 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        MessageComposerView view = findViewById(R.id.reminder_composer_view);
-//        MessageComposerViewManager.newInstance(this, view).bindContentView(new ReminderContent(this))
-//                .bindComposerView(new ReminderComposerEdit(this))
-//                .bindAccessoryView(new ReminderEditExtraContent(this))
-//                .bindAccessoryView(new TestEdit(this)).build();
     }
-//
-//    public void onClick(View v){
-//        Intent intent;
-//        switch (v.getId()){
-//            case R.id.show_normal_picker:
-//                intent = new Intent(MainActivity.this, NormalNumberPicker.class);
-//                startActivity(intent);
-//                break;
-//            case R.id.show_unlimited_number_picker:
-//                intent = new Intent(MainActivity.this, UnlimitedNumberPicker.class);
-//                startActivity(intent);
-//                break;
-//            case R.id.show_week_day_picker:
-//                intent = new Intent(MainActivity.this, WeekDayPicker.class);
-//                startActivity(intent);
-//                break;
-//            case R.id.show_day_picker:
-//                intent = new Intent(MainActivity.this, DayPicker.class);
-//                startActivity(intent);
-//                break;
-//            case R.id.show_am_pm_picker:
-//                intent = new Intent(MainActivity.this, AMPMPicker.class);
-//                startActivity(intent);
-//                break;
-//            default:
-//                break;
-//        }
-//    }
 
-
+    public void onClick(View v){
+        Intent intent;
+        switch (v.getId()){
+            case R.id.show_normal_picker:
+                intent = new Intent(MainActivity.this, NormalNumberPicker.class);
+                startActivity(intent);
+                break;
+            case R.id.show_unlimited_number_picker:
+                intent = new Intent(MainActivity.this, UnlimitedNumberPicker.class);
+                startActivity(intent);
+                break;
+            case R.id.show_week_day_picker:
+                intent = new Intent(MainActivity.this, WeekDayPicker.class);
+                startActivity(intent);
+                break;
+            case R.id.show_day_picker:
+                intent = new Intent(MainActivity.this, DayPicker.class);
+                startActivity(intent);
+                break;
+            case R.id.show_am_pm_picker:
+                intent = new Intent(MainActivity.this, AMPMPicker.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
 }
